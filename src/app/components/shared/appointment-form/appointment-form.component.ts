6835237f2755b7656102905913f95c31c13b129c
Miguel Velazquez <mavodeveloper@gmail.com>
2021-06-01 13:01:43 -0500

    Se valida el formato de las fechas

--- src/app/components/shared/appointment-form/appointment-form.component.ts
+++ src/app/components/shared/appointment-form/appointment-form.component.ts
@@ -14,6 +14,8 @@ export class AppointmentFormComponent implements OnInit {
   @Input() userAppointment: Usuario;
   startValue: Date | null = null;
   endValue: Date | null = null;
+  currentDate: Date = new Date();
+  dates: Date[];
   @ViewChild('endDatePicker') endDatePicker!: NzDatePickerComponent;
   inputValue?: string;
   alerta: Alert = new Alert();
@@ -22,30 +24,16 @@ export class AppointmentFormComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  disabledStartDate = (startValue: Date): boolean => {
-    if (!startValue || !this.endValue) {
-      return false;
-    }
-    return startValue.getTime() > this.endValue.getTime();
-  };
-
-  disabledEndDate = (endValue: Date): boolean => {
-    if (!endValue || !this.startValue) {
-      return false;
-    }
-    return endValue.getTime() <= this.startValue.getTime();
-  };
-
-  handleStartOpenChange(open: boolean): void {
-    if (!open) {
-      this.endDatePicker.open();
-    }
-    // console.log('handleStartOpenChange', open);
+  diffMinutes(date2: Date, date1: Date): number {
+    let diff = (date2.getTime() - date1.getTime()) / 1000;
+    diff /= 60;
+    return Math.abs(Math.round(diff));
   }
 
-  handleEndOpenChange(open: boolean): void {
-    // console.log('handleEndOpenChange', open);
+  dateInPast(firstDate: Date, secondDate: Date): boolean {
+    return firstDate.getTime() <= secondDate.getTime();
   }
+
   saveAppointment() {
     const appointment: Appointment = {
       idUser: this.userAppointment.id,
@@ -54,21 +42,102 @@ export class AppointmentFormComponent implements OnInit {
       description: this.inputValue,
     };
     // console.log(appointment);
-    this.appointmentService
-      .addAppointment(appointment)
-      .then(() => {
+    const dateInPast = this.dateInPast(this.startValue, this.currentDate);
+    if (!dateInPast) {
+      const saveDates = this.datesValidation();
+      if (saveDates) {
+        // console.log('Se puede guardar');
+        this.alerta.loading();
+        this.appointmentService
+          .addAppointment(appointment)
+          .then(() => {
+            this.alerta.mostrarAlerta(
+              'success',
+              'La cita fue registrada!',
+              '',
+              1000
+            );
+            this.startValue;
+            this.endValue;
+            this.inputValue;
+          })
+          .catch(() => {
+            this.alerta.mostrarAlerta(
+              'error',
+              'No se registro la cita!',
+              '',
+              1000
+            );
+          });
+      }
+    } else {
+      this.dates = [];
+    }
+  }
+
+  onChange(event: any) {
+    console.log('On change', event);
+  }
+  onCalendarChange(event: any) {
+    // console.log('On calendar change', event);
+  }
+  onOk(event: Date[]) {
+    console.log('On ok', event);
+    this.startValue = event[0];
+    this.endValue = event[1];
+
+    if (this.startValue && this.endValue) {
+      const dateInPast = this.dateInPast(this.startValue, this.currentDate);
+      if (!dateInPast) {
+        const saveDates = this.datesValidation();
+        if (saveDates) {
+          // console.log('Se puede guardar');
+        }
+      } else {
+        this.dates = [];
+      }
+    }
+  }
+
+  datesValidation(): boolean {
+    if (this.startValue && this.endValue) {
+      const date1 = new Date(this.startValue).getMinutes();
+      const date2 = new Date(this.endValue).getMinutes();
+
+      if (date1 === 0 || (date1 === 30 && date2 === 0) || date2 === 30) {
+        console.log(date1, date2);
+        if (this.startValue && this.endValue) {
+          const minutes = this.diffMinutes(this.endValue, this.startValue);
+          // console.log(minutes);
+          if (minutes === 30) {
+            // console.log('Se puede guardar la cita');
+            return true;
+          } else {
+            // console.log('No se puede guardar la cita');
+            this.startValue = null;
+            this.endValue = null;
+            this.dates = [];
+            this.alerta.mostrarAlerta(
+              'error',
+              'Hubo un error!',
+              'Las horas de la cita deben ser como en el siguiente ejemplo: 14:00 o 14:30. Y tener un lapso de tiempo de media hora.',
+              5000
+            );
+            return false;
+          }
+        }
+      } else {
+        this.startValue = null;
+        this.endValue = null;
+        this.dates = [];
         this.alerta.mostrarAlerta(
-          'success',
-          'La cita fue registrada!',
-          '',
-          1000
+          'error',
+          'Hubo un error!',
+          'Las horas de la cita deben ser como en el siguiente ejemplo: 14:00 o 14:30. Y tener un lapso de tiempo de media hora.',
+          5000
         );
-        this.startValue;
-        this.endValue;
-        this.inputValue;
-      })
-      .catch(() => {
-        this.alerta.mostrarAlerta('error', 'No se registro la cita!', '', 1000);
-      });
+        return false;
+      }
+    }
   }
 }
